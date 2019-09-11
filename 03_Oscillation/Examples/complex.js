a3e7a823ee7115edefc7a5f842a70367f650d9db
Joerg Blumtritt <joerg.blumtritt@mediagnosis.de>
2019-09-11 12:53:23 +0400

    Create complex.js

---
+++ 03_Oscillation/Examples/complex.js
@@ -0,0 +1,19 @@
+// a super simple class for complex numbers in JS
+
+class Complex {
+  constructor(a, b) {
+    this.re = a;
+    this.im = b;
+  }
+  
+  add(c) {
+    this.re += c.re;
+    this.im += c.im;
+  }
+  
+  mult(c) {
+    const re = this.re * c.re - this.im * c.im;
+    const im = this.re * c.im + this.im * c.re;
+    return new Complex(re, im);
+  }
+}
