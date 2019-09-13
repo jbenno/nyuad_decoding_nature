cdf59b61b23a9a41836a8582cdd9bb0e8250469c
Joerg Blumtritt <joerg.blumtritt@mediagnosis.de>
2019-09-13 20:44:37 +0400

    updated with all the missing basic functions

--- 03_Oscillation/Examples/complex.js
+++ 03_Oscillation/Examples/complex.js
@@ -1,19 +1,52 @@
-// a super simple class for complex numbers in JS
-
 class Complex {
   constructor(a, b) {
     this.re = a;
     this.im = b;
   }
   
-  add(c) {
+  addc(c) {
+    // adds two complex numbers
     this.re += c.re;
     this.im += c.im;
   }
   
-  mult(c) {
+  multc(c) {
+    // multiplies two complex numbers
     const re = this.re * c.re - this.im * c.im;
     const im = this.re * c.im + this.im * c.re;
     return new Complex(re, im);
   }
+  
+  mults(x) {
+    // multiplies a complex number with a scalar
+    const re = x * this.re;
+    const im = x * this.im;
+    return new Complex(re,im);
+  }
+  
+  conjc() {
+    // creates the complex conjugate
+    const re = this.re;
+    const im = - this.im;
+    return new Complex(re,im);
+  }
+  
+  absc() {
+    // the absolute (scalar) value of a complex number
+    let a;
+    const re = this.re;
+    const im = this.im;
+    a = sqrt((re)^2 + (im)^2);
+    return a;
+  }
+    
+}
+
+// declaration of i
+const i= new Complex(0,1);
+
+// complex exponential
+function expc(x) {
+  let expcx = new Complex(cos(x),sin(x));
+  return expcx;
 }
