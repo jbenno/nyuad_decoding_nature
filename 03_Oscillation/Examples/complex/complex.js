7036d19e6819231dae684ccf51c23471e1472262
Joerg Blumtritt <joerg.blumtritt@mediagnosis.de>
2019-09-20 10:48:30 +0400

    a few updates

--- 03_Oscillation/Examples/complex/complex.js
+++ 03_Oscillation/Examples/complex/complex.js
@@ -6,8 +6,9 @@ class Complex {
   
   addc(c) {
     // adds two complex numbers
-    this.re += c.re;
-    this.im += c.im;
+    const re = this.re + c.re;
+    const im = this.im + c.im;
+    return new Complex(re, im);
   }
   
   multc(c) {
@@ -36,11 +37,33 @@ class Complex {
     let a;
     const re = this.re;
     const im = this.im;
-    a = sqrt((re)^2 + (im)^2);
+    a = sqrt((re)**2 + (im)**2);
     return a;
   }
-    
+
 }
+
+function argc(c) {
+      let theta = 0;
+      const re = c.re;
+      const im = c.im;    
+      const ab = c.absc();
+    
+      if (im !=0 || re > 0) {
+        theta = 2* atan(im/(ab+re));
+      } else if (re < 0 && im ==0) {
+        theta = PI;
+        } else {
+        theta = 0;
+      }
+      if (theta >= 0) {
+          return theta;
+        } else {
+          theta = 2*PI+theta;
+          return theta;
+        }
+      
+    }
 
 // declaration of i
 const i= new Complex(0,1);
